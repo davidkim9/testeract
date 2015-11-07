1aa0c1c0a7cef22ada3cf1b9848f6fd986d9407c
David Kim <davidk@halopowered.com>
2015-11-07 13:16:33 -0500

    Init

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+var app = express();
+
+app.get('/', function (req, res) {
+  var child;
+  var command = 'tesseract -l eng 123.jpg test';
+  child = exec(command, function (error, stdout, stderr) {
+    // sys.print('stdout: ' + stdout);
+    // sys.print('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    res.send('Hello World!' + stdout);
+  });
+  
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});

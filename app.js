c04cd7736d838a57d95c207b06b94563cf388cee
David Kim <davidk@halopowered.com>
2015-11-07 15:25:26 -0500

    upload

--- app.js
+++ app.js
@@ -5,10 +5,47 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var shortid = require('shortid');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.jpg') //Appending .jpg
+  }
+})
+
+var upload = multer({ storage: storage })
 
 var app = express();
 
+function runTesseract(fileName, cb){
+  var child;
+  var command = "tesseract " + fileName + " stdout";
+  console.log("Running command: " + command);
+  child = exec(command, function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    console.log("Tesseract complete!");
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    cb(stdout);
+  });
+}
+
+app.post('/upload', upload.single('image'), function(req, res){
+  var file = req.file;
+  runTesseract(file.path, function(text){
+    res.send(text);
+  });
+  console.log(req.body, req.file);
+  // return res.send("UPLOAD REQUEST");
+});
+
 app.get('/', function (req, res) {
+  return res.sendFile(__dirname + '/views/index.html');
   //http://testeract.herokuapp.com/?url=http://www.codeproject.com/KB/recipes/OCR-Chain-Code/image012.jpg
   var url = req.query.url;
   try{
@@ -20,16 +57,8 @@ app.get('/', function (req, res) {
           file.write(data);
       }).on('end', function() {
           file.end();
-          var child;
-          var command = "tesseract " + fileName + " stdout";
-          child = exec(command, function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            console.log("Tesseract complete!");
-            if (error !== null) {
-              console.log('exec error: ' + error);
-            }
-            res.send(stdout);
+          runTesseract(fileName, function(text){
+            res.send(text);
           });
       });
     }

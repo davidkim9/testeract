2882432f82e371056d1d10ddc96c9f33a868869b
Adam Lee <adam@ninjastack.com>
2015-11-07 14:05:20 -0500

    try read file

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var sys = require('sys')
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 var app = express();
 
 app.get('/', function (req, res) {
   var child;
-  var command = 'tesseract 123.jpg stdout';
+  var command = 'tesseract -l eng 123.jpg test';
   child = exec(command, function (error, stdout, stderr) {
     // sys.print('stdout: ' + stdout);
     // sys.print('stderr: ' + stderr);
     if (error !== null) {
-      console.log('exec error: ' + error);
+      return console.log('exec error: ' + error);
+    } else {
+      fs.readFile('test.txt', function (err, contents) {
+        if(err) return res.send('FAIL');
+        return res.send(contents);
+      });
     }
-    res.send(stdout);
+
   });
+
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {

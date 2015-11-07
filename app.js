3dbe1615c09caccf0f758de1d0f7f501699a7572
David Kim <davidk@halopowered.com>
2015-11-07 18:00:11 -0500

    Ignore nonjpg

--- app.js
+++ app.js
@@ -23,78 +23,82 @@ var upload = multer({ storage: storage })
 var app = express();
 
 function makeBinaryImage(fileName, cb){
-  var jpegData = fs.readFileSync(fileName);
-  var rawImageData = jpeg.decode(jpegData);
-  var histogram = [];
-  for(var i = 0; i < 256; i++) histogram[i] = 0;
-  var count = 0;
-  for(var h = 0; h < rawImageData.height; h++){
-    for(var w = 0; w < rawImageData.width; w++){
-      var avg = 0;
-      avg += rawImageData.data[count];
-      avg += rawImageData.data[count+1];
-      avg += rawImageData.data[count+2];
-      avg += rawImageData.data[count+3];
-      avg = Math.floor(avg / 4);
-      if(histogram[avg]){
-        histogram[avg]++;
-      }else{
-        histogram[avg] = 1;
+  try{
+    var jpegData = fs.readFileSync(fileName);
+    var rawImageData = jpeg.decode(jpegData);
+    var histogram = [];
+    for(var i = 0; i < 256; i++) histogram[i] = 0;
+    var count = 0;
+    for(var h = 0; h < rawImageData.height; h++){
+      for(var w = 0; w < rawImageData.width; w++){
+        var avg = 0;
+        avg += rawImageData.data[count];
+        avg += rawImageData.data[count+1];
+        avg += rawImageData.data[count+2];
+        avg += rawImageData.data[count+3];
+        avg = Math.floor(avg / 4);
+        if(histogram[avg]){
+          histogram[avg]++;
+        }else{
+          histogram[avg] = 1;
+        }
+        count+=4;
       }
-      count+=4;
     }
-  }
 
-  //Best threshold
-  var start = 0;
-  var end = histogram.length - 1;
-  var left = 0;
-  var right = 0;
-  while(start < end) {
-    if(left < right) {
-      //More information on right side, add weight to left
-      left += histogram[start];
-      start++;
-    }else if(left > right){
-      //More information on left side, add weight to right
-      right += histogram[end];
-      end--;
-    }else{
-      //Both equal, don't bother adding since they're equal
-      start++;
-      end--;
-    }
-  }
-
-  count = 0;
-  for(var h = 0; h < rawImageData.height; h++){
-    for(var w = 0; w < rawImageData.width; w++){
-      var avg = 0;
-      avg += rawImageData.data[count];
-      avg += rawImageData.data[count+1];
-      avg += rawImageData.data[count+2];
-      avg += rawImageData.data[count+3];
-      avg = Math.floor(avg / 4);
-      if(avg < start){
-        rawImageData.data[count] = 0;
-        rawImageData.data[count+1] = 0;
-        rawImageData.data[count+2] = 0;
-        rawImageData.data[count+3] = 0;
+    //Best threshold
+    var start = 0;
+    var end = histogram.length - 1;
+    var left = 0;
+    var right = 0;
+    while(start < end) {
+      if(left < right) {
+        //More information on right side, add weight to left
+        left += histogram[start];
+        start++;
+      }else if(left > right){
+        //More information on left side, add weight to right
+        right += histogram[end];
+        end--;
       }else{
-        rawImageData.data[count] = 255;
-        rawImageData.data[count+1] = 255;
-        rawImageData.data[count+2] = 255;
-        rawImageData.data[count+3] = 255;
+        //Both equal, don't bother adding since they're equal
+        start++;
+        end--;
       }
-      count+=4;
     }
+
+    count = 0;
+    for(var h = 0; h < rawImageData.height; h++){
+      for(var w = 0; w < rawImageData.width; w++){
+        var avg = 0;
+        avg += rawImageData.data[count];
+        avg += rawImageData.data[count+1];
+        avg += rawImageData.data[count+2];
+        avg += rawImageData.data[count+3];
+        avg = Math.floor(avg / 4);
+        if(avg < start){
+          rawImageData.data[count] = 0;
+          rawImageData.data[count+1] = 0;
+          rawImageData.data[count+2] = 0;
+          rawImageData.data[count+3] = 0;
+        }else{
+          rawImageData.data[count] = 255;
+          rawImageData.data[count+1] = 255;
+          rawImageData.data[count+2] = 255;
+          rawImageData.data[count+3] = 255;
+        }
+        count+=4;
+      }
+    }
+
+    var jpegImageData = jpeg.encode(rawImageData, 50);
+
+    var file = fs.createWriteStream(fileName);
+    file.write(jpegImageData.data);
+    file.end();
+  }catch(e){
+    console.log(e);
   }
-
-  var jpegImageData = jpeg.encode(rawImageData, 50);
-
-  var file = fs.createWriteStream(fileName);
-  file.write(jpegImageData.data);
-  file.end();
   cb();
 }
 
